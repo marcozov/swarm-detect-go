2eae06a7b1ff46eb030075e671c39b5d996721c8
marcozov <marco.zoveralli@gmail.com>
2018-10-01 18:59:28 +0200

    modified server to fix reception bug

--- server/connectivity.go
+++ server/connectivity.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-	"log"
+	//"log"
 	"net"
 	"os"
-	"os/signal"
-	"syscall"
+	//"os/signal"
+	//"syscall"
+	"fmt"
 )
 
+/*
 func echoServer(c net.Conn) {
 	for {
-		buf := make([]byte, 65536)
+		fmt.Println("reading...")
+		buf := make([]byte, 4096)
 		nr, err := c.Read(buf)
+		fmt.Println("READ! **********")
 		if err != nil {
+			fmt.Sprintf("Error: %s\n", err)
 			return
 		}
 
@@ -20,8 +25,11 @@ func echoServer(c net.Conn) {
 		println("Server got:", string(data))
 		_, err = c.Write(data)
 		if err != nil {
+			fmt.Sprintf("Error: %s\n", err)
 			log.Fatal("Writing client error: ", err)
 		}
+
+		fmt.Println("after server got")
 	}
 }
 
@@ -42,11 +50,50 @@ func main() {
 	}(ln, sigc)
 
 	for {
+		fmt.Println("main method")
 		fd, err := ln.Accept()
 		if err != nil {
+			fmt.Println("accept error: ", err)
 			log.Fatal("Accept error: ", err)
 		}
 
 		go echoServer(fd)
 	}
+}
+*/
+
+func main() {
+	l, err := net.ListenUnix("unix",  &net.UnixAddr{"/tmp/go.sock", "unix"})
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove("/tmp/go.sock")
+
+
+	connectionCreation:
+
+	conn, err := l.AcceptUnix()
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		//conn, err := l.AcceptUnix()
+		//if err != nil {
+		//	panic(err)
+		//}
+		var buf [4096]byte
+		n, err := conn.Read(buf[:])
+		if err != nil {
+			fmt.Println("Error in reading data:", err)
+			conn.Close()
+			goto connectionCreation
+			//panic(err)
+
+		}
+		fmt.Printf("server got: %s\n(%d bytes)", string(buf[:n]), n);
+		//conn.Close()
+	}
+
+	conn.Close()
 }
